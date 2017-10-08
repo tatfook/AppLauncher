470790c2bd2dc4550db7575d71b19566b776467e
LiXizhi <lxz1982@hotmail.com>
2017-10-09 02:09:00 +0800

    add embedded files

---
+++ npl_packages/AppLauncher/script/AppLauncher/main.lua
@@ -0,0 +1,2 @@
+-- main loop
+-- LiXizhi
--- src/AppLauncher.cpp
+++ src/AppLauncher.cpp
@@ -6,6 +6,8 @@
 // Date:	2017.10.2
 //-----------------------------------------------------------------------------
 #include "AppLauncher.h"
+#include "IO/ResourceEmbedded.h"
+
 using namespace ParaEngine;
 
 //-----------------------------------------------------------------------------
@@ -18,13 +20,18 @@ INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR lpCmdLine, INT )
 	std::string sAppCmdLine;
 	if(lpCmdLine)
 		sAppCmdLine = lpCmdLine;
-	
+	ADD_RESOURCE(":AppLauncherPackage", AppLauncher_zip);
+
 	auto pParaEngine = ParaEngine::GetCOREInterface();
 	if (pParaEngine)
 	{
 		auto pParaEngineApp = pParaEngine->CreateApp();
+
 		if (pParaEngineApp == 0)
 			return E_FAIL;
+
+		// TODO: load archive from ":AppLauncherPackage"
+		// pParaEngineApp->LoadNPLPackage();
 
 		if (pParaEngineApp->StartApp(lpCmdLine) != S_OK)
 			return E_FAIL;
