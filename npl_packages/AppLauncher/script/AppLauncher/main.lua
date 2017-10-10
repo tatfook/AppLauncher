0aaccf1fd39c41525811e1d88b05ed10e57679eb
LiXizhi <lixizhi@yeah.net>
2017-10-10 15:10:24 +0800

    support dumping files

--- npl_packages/AppLauncher/script/AppLauncher/main.lua
+++ npl_packages/AppLauncher/script/AppLauncher/main.lua
@@ -1,5 +1,19 @@
 -- main loop
 -- LiXizhi
 
+NPL.load("(gl)script/ide/System/System.lua")
+
+local function DumpUsedFiles()
+	local files = __rts__:GetStats({loadedfiles={}})
+
+	for filename, status in pairs(files.loadedfiles) do
+		if(ParaIO.DoesFileExist(filename)) then
+			echo(filename)
+		end
+	end
+end
+
+DumpUsedFiles();
+
 log("1111111111111111111\n")
 ParaGlobal.Exit(0);
