8e25b34ae29ae9e077b8f41ddbb28800deb776bd
Triple-GGG <3431714864@qq.com>
2017-11-30 15:59:58 +0800

    add tips text to the TipsWindow

--- npl_packages/AppLauncher/script/AppLauncher/main/TipsWindow.lua
+++ npl_packages/AppLauncher/script/AppLauncher/main/TipsWindow.lua
@@ -10,7 +10,7 @@ local MainWindow = commonlib.gettable("AppLauncher.MainWindow")
 local TipsWindow = commonlib.gettable("AppLauncher.TipsWindow")
 
 function TipsWindow.OnInit()
-
+    TipsWindow.page = document:GetPageCtrl()
 end
 
 function TipsWindow.ShowPage()
@@ -22,6 +22,23 @@ function TipsWindow.ShowPage()
 	});
 
     TipsWindow.window = window
+
+    TipsWindow.ShowTips()
+end
+
+function TipsWindow.ShowTips()
+    if TipsWindow.page then
+        local node = MainWindow.GetSelectedNode()
+        if node then
+            local id = node.id
+            local a = MainWindow.CreateOrGetAssetsManager(id)
+            local cur_version = a:getCurVersion()
+            local latest_version = a:getLatestVersion()
+
+            local s = string.format("新版本v%s已经提供下载，你目前的版本是v%s，现在需要更新吗？", cur_version, latest_version)
+            TipsWindow.page:SetValue("tips_txt", s)
+        end
+    end
 end
 
 function TipsWindow.OnJump()
