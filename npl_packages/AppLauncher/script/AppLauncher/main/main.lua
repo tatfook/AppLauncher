9a6ec020c4ef91025fa335f40b9946c59a1dd6bc
Triple-GGG <3431714864@qq.com>
2018-01-12 14:34:03 +0800

    固定窗口大小

--- npl_packages/AppLauncher/script/AppLauncher/main/main.lua
+++ npl_packages/AppLauncher/script/AppLauncher/main/main.lua
@@ -3,6 +3,7 @@
 
 NPL.load("(gl)script/ide/System/System.lua")
 local launcher_debug = ParaEngine.GetAppCommandLineByParam("launcher_debug", nil);
+local token = ParaEngine.GetAppCommandLineByParam("token", nil);
 
 local function DumpUsedFiles()
 	local files = __rts__:GetStats({loadedfiles={}})
@@ -21,17 +22,24 @@ local function ShowLauncher()
     NPL.load("(gl)script/AppLauncher/lang/lang.lua");
 	NPL.load("script/AppLauncher/main/PageLoader.lua");
     local PageLoader = commonlib.gettable("AppLauncher.PageLoader");
-    if(launcher_debug)then
+    --if(launcher_debug)then
         PageLoader.ShowAppPage();
-    else
-        PageLoader.CheckVersion()
-    end
+    --else
+        --PageLoader.CheckVersion()
+    --end
 end
 
 local main_state = nil;
 NPL.this(function()
 	if(not main_state) then
 		main_state = "inited";
+
+        local att = ParaEngine.GetAttributeObject()
+        att:SetField("ScreenResolution", {1020, 595})
+        att:CallField("UpdateScreenMode")
+
+        att:SetField("LockWindowSize", true)
+
 		ShowLauncher();
 	end
 end);
